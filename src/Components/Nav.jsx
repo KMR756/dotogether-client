ec28f4f37af6a84e4dfbad8abff7278619550c2f
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2025-06-24 04:01:45 +0600

    droupdown menu created for user profile

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,57 +1,102 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import userImg from "../assets/user.png";
 import DarkMoodBtn from "./DarkMoodBtn";
 import { Link } from "react-router";
 
 const Nav = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
   return (
-    <>
-      <nav className="bg-[#4ED7F1] dark:bg-[#1A1A1D] ">
-        <div className="flex justify-between w-[95%] md:w-[90%] lg:w-10/12 mx-auto py-6">
-          <div className="">
-            <Link to={"/"} className="flex  flex-col">
-              <span className="self-center flex  caprasimo font-bold text-xs md:text-3xl lg:text-4xl xl:text-5xl  text-[#3A0519] dark:text-[#FF6363]">
-                DoTogether
-              </span>
-              <span className="flex justify-end caprasimo mt-.5 md:mt-1 lg:mt-2 xl:mt-3 text-[7px] md:text-xl xl:text-2xl text-white">
-                Together We Do.....
-              </span>
-            </Link>
-          </div>
-          <div className=" flex items-center ">
-            <ul>
-              <li>
-                <Link to={"/upcomingevents"}>
-                  <button className="relative inline-flex items-center justify-center p-0.5 mb-0 md:mb-2  me-2 overflow-hidden  font-medium text-[#3A0519] dark:text-[#FBFBFB] rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white  ">
-                    <span className="relative md:font-semibold inter text-[10px] md:text-xl lg:text-2xl xl:text-3xl px-1 md:px-2 lg:px-5 py-1 md:py-2 lg:py-2.5 transition-all ease-in duration-75 bg-[#FFDCDC] dark:bg-[#A2678A] rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
-                      Upcoming Events
-                    </span>
-                  </button>
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="flex items-center  ">
-            <button>
-              <img
-                src={userImg}
-                className=" w-7 md:w-13 h-7 md:h-13 mr-2 md:mr-4"
-                alt=""
-              />
-            </button>
-            <Link to={"/login"}>
-              <button
-                type="button"
-                className="text-white font-semibold bg-gradient-to-r inter text-xs md:text-2xl from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br  shadow-lg shadow-red-500/20 dark:shadow-lg dark:shadow-red-800/20 rounded-lg px-1 md:px-5 py-1 md:py-2.5 text-center me-2 "
-              >
-                Log In
-              </button>
-            </Link>
-            <DarkMoodBtn />
-          </div>
+    <nav className="bg-[#4ED7F1] dark:bg-[#1A1A1D] ">
+      <div className="flex justify-between w-[95%] md:w-[90%] lg:w-10/12 mx-auto py-6">
+        <div>
+          <Link to={"/"} className="flex  flex-col">
+            <span className="self-center flex caprasimo font-bold text-xs md:text-3xl lg:text-4xl xl:text-5xl text-[#3A0519] dark:text-[#FF6363]">
+              DoTogether
+            </span>
+            <span className="flex justify-end caprasimo mt-0.5 md:mt-1 lg:mt-2 xl:mt-3 text-[7px] md:text-xl xl:text-2xl text-white">
+              Together We Do.....
+            </span>
+          </Link>
         </div>
-      </nav>
-    </>
+
+        <div className="flex items-center">
+          <ul>
+            <li>
+              <Link to={"/upcomingevents"}>
+                <button className="relative inline-flex items-center justify-center p-0.5 mb-0 md:mb-2 me-2 overflow-hidden font-medium text-[#3A0519] dark:text-[#FBFBFB] rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 hover:text-white">
+                  <span className="relative md:font-semibold inter text-[10px] md:text-xl lg:text-2xl xl:text-3xl px-1 md:px-2 lg:px-5 py-1 md:py-2 lg:py-2.5 transition-all ease-in duration-75 bg-[#FFDCDC] dark:bg-[#A2678A] rounded-md group-hover:bg-transparent group-hover:dark:bg-transparent">
+                    Upcoming Events
+                  </span>
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex items-center space-x-2 relative" ref={dropdownRef}>
+          <button onClick={() => setDropdownOpen(!dropdownOpen)}>
+            <img
+              src={userImg}
+              className="w-7 md:w-13 h-7 md:h-13 mr-2 md:mr-4"
+              alt="User"
+            />
+          </button>
+
+          {dropdownOpen && (
+            <div className="absolute right-0 md:right-33 lg:right-21  top-8 md:top-16 lg:top-18 xl:top-20 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-30 md:w-40 dark:bg-gray-700">
+              <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+                <li>
+                  <a
+                    href="#"
+                    className="block px-2 md:px-3 py-1 md:py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Create Event
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block px-2 md:px-3 py-1 md:py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Manage Events
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="block px-2 md:px-3 py-1 md:py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    Joined Events
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
+
+          <Link to={"/login"}>
+            <button
+              type="button"
+              className="text-white font-semibold bg-gradient-to-r inter text-xs md:text-2xl from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br shadow-lg shadow-red-500/20 dark:shadow-lg dark:shadow-red-800/20 rounded-lg px-1 md:px-5 py-1 md:py-2.5 text-center"
+            >
+              Log In
+            </button>
+          </Link>
+          <DarkMoodBtn />
+        </div>
+      </div>
+    </nav>
   );
 };
 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqEd3kiQf4rha5p43wo3psrr8rLfuQQt4",
+  authDomain: "do-together-743ba.firebaseapp.com",
+  projectId: "do-together-743ba",
+  storageBucket: "do-together-743ba.firebasestorage.app",
+  messagingSenderId: "107892939346",
+  appId: "1:107892939346:web:4b80f6405261a348201d8b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
